0c2ae145ca270ddfed4b4d50289add140c65ec9e
Tuan Anh Nguyen <anh.nt@zinza.com.vn>
2019-12-17 16:57:02 +0700

    apply json

--- main.go
+++ main.go
@@ -2,34 +2,41 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 	kafka "github.com/segmentio/kafka-go"
 )
 
+// User :
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 // HandlePost :
 func HandlePost(w http.ResponseWriter, r *http.Request) {
-	kafkaURL := os.Getenv("KAFKA_URL")
-	topic := os.Getenv("KAFKA_TOPIC")
-	// kafkaURL := "localhost:9092"
-	// topic := "user-topic"
+	// kafkaURL := os.Getenv("KAFKA_URL")
+	// topic := os.Getenv("KAFKA_TOPIC")
+	kafkaURL := "localhost:29092"
+	topic := "user-topic"
 	conn := getKafkaWriter(kafkaURL, topic)
 	defer conn.Close()
 
-	body, err := ioutil.ReadAll(r.Body)
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	v, err := json.Marshal(user)
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
 	msg := kafka.Message{
 		Key:   []byte(fmt.Sprintf("address-%s", r.RemoteAddr)),
-		Value: body,
+		Value: v,
 	}
 
 	_, err = conn.WriteMessages(msg)
@@ -43,7 +50,11 @@ func HandlePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func getKafkaWriter(kafkaURL, topic string) *kafka.Conn {
-	conn, _ := kafka.DialLeader(context.Background(), "tcp", kafkaURL, topic, 0)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", kafkaURL, topic, 0)
+
+	if err != nil {
+		log.Panic("Connection Err", err)
+	}
 
 	return conn
 }
